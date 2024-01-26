84fd5a02de2b019138faf658ee860a122465dee7
MartinDotNet <github@my2cents.co.uk>
2024-01-26 13:26:08 +0000

    Added debug logging to stdout

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.Tracing;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
@@ -15,7 +16,29 @@ builder.Services.AddOpenTelemetry()
 
 var app = builder.Build();
 
+var openTelemetryDebugLogger = app.Services.GetRequiredService<LoggingOpenTelemetryListener>();
+
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/live", () => "ok");
 
 app.Run();
+
+public class LoggingOpenTelemetryListener : EventListener
+{
+    private readonly ILogger<LoggingOpenTelemetryListener> _logger;
+ 
+    public LoggingOpenTelemetryListener(ILogger<LoggingOpenTelemetryListener> logger)
+    {
+        _logger = logger;
+    }
+    protected override void OnEventSourceCreated(EventSource eventSource)
+    {
+        if (eventSource.Name.StartsWith("OpenTelemetry"))
+            EnableEvents(eventSource, EventLevel.Error);
+    }
+ 
+    protected override void OnEventWritten(EventWrittenEventArgs eventData)
+    {
+        _logger.LogWarning(eventData.Message, eventData.Payload?.Select(p => p?.ToString())?.ToArray()!);
+    }
+}
